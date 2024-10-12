b62e5baf196531ad541db92e3e7168caf9cfe97e
Abhay <zzxxcc6386@gmail.com>
2024-10-12 23:50:39 +0530

    a

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const PORT =process.env.PORT  || 3000;
 
 // Root route
 app.get('/', (req, res) => {
-  res.send('Hello, World! Abhay Pratap SIngh UPDATED');
+  res.send('Hello, World! Abhay Pratap SIngh UPDATED update for EC2');
 });
 
 // Start server
